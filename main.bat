b9b1ffb8aaa6b4d83e5aa93c67e32b5bce9c13f3
ThomasLMM <113507303+ThomasLMM@users.noreply.github.com>
2022-09-14 10:37:47 +0200

    Beautify code

--- main.bat
+++ main.bat
@@ -11,7 +11,7 @@ ECHO Veuillez saisir le nom exact de la connexion WiFi, en respectant la casse. 
 ECHO.
 ECHO.
 
-SET /p SSID=Nom de la connexion wifi : 
+SET /P SSID=Nom de la connexion wifi : 
 
 FOR /f "delims=" %%A IN ('"netsh wlan show profile name=%SSID% key=clear | findstr Key"') DO SET wifi_key=%%A
 IF /I "%wifi_key%" EQU "" goto :ErrorEmpty
@@ -24,20 +24,20 @@ ECHO **********************************************************
 ECHO.
 
 :choice
-set /P c=Voulez-vous copier le mot de passe [O/N]?
-if /I "%c%" EQU "O" goto :oui
-if /I "%c%" EQU "N" goto :non
+SET /P c=Voulez-vous copier le mot de passe [O/N]?
+IF /I "%c%" EQU "O" goto :yes
+IF /I "%c%" EQU "N" goto :no
 goto :choice
 
 
-:oui 
+:yes
 ECHO %wifi_key% | clip
 ECHO SUCCÈS : Mot de passe copié dans le presse-papier.
 
 PAUSE
 EXIT
 
-:non
+:no
 EXIT
 
 :ErrorEmpty
